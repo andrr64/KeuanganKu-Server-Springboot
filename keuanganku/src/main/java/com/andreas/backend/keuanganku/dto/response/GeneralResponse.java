420dc27276b1a9dcd49ba1b1ef96ef211618f12b
andrr64 <derzandrr03@gmail.com>
2025-07-13 23:26:09 +0700

    Add GeneralResponse DTO for standardized API responses

---
+++ keuanganku/src/main/java/com/andreas/backend/keuanganku/dto/response/GeneralResponse.java
@@ -0,0 +1,18 @@
+package com.andreas.backend.keuanganku.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GeneralResponse<T> {
+    private String message;
+    private T data;
+
+    // Constructor tanpa data
+    public GeneralResponse(String message) {
+        this.message = message;
+    }
+}
